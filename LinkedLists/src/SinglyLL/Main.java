fb64df095eda346d1da8cbda570f0889699bd003
18simran <mewannabeauthentic@gmail.com>
2024-05-22 23:01:18 +0530

    Added

--- LinkedLists/src/SinglyLL/Main.java
+++ LinkedLists/src/SinglyLL/Main.java
@@ -6,20 +6,20 @@ public class Main
     {
         SinglyLL list = new SinglyLL();
         list.insertFirst(5);
-        list.insertFirst(10);
-        list.insertFirst(15);
-        list.insertFirst(20);
-        list.display();
+//        list.insertFirst(10);
+//        list.insertFirst(15);
+//        list.insertFirst(20);
+//        list.display();
 //        list.insert(25,3);
 //        list.display();
-//        list.DeleteFirst();
-//        System.out.println();
-//        list.display();
+        list.DeleteFirst();
+        System.out.println();
+        list.display();
 //        list.DeleteLast();
 //        System.out.println();
 //        list.display();
-        System.out.println();
-        list.Delete(3);
-        list.display();
+//        System.out.println();
+//        list.Delete(3);
+//        list.display();
     }
 }
