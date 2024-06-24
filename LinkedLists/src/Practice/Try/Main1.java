39b68a66ffdf1da41fe555a962c5f6da8062ba25
18simran <mewannabeauthentic@gmail.com>
2024-06-24 21:35:19 +0530

    Added

--- LinkedLists/src/Practice/Try/Main1.java
+++ LinkedLists/src/Practice/Try/Main1.java
@@ -2,14 +2,17 @@ package Practice.Try;
 
 public class Main1
 {
-    public static void main(String[] args) {
-    LLpractice list = new LLpractice();
-    list.insertFirst(35);
-    list.insertFirst(40);
-    list.insertLast(100);
-    list.insertFirst(14);
-
-    list.display();
+    public static void main(String[] args)
+    {
+     LLpractice n = new LLpractice();
+     n.insertFirst(10);
+     n.insertFirst(15);
+     n.insertFirst(20);
+     n.insertLast(25);
+     n.insertFirst(20);
+     n.deleteFirst();
+     n.deleteLast();
+     n.display();
     }
 
 }
