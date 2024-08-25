e74c8a7c4ea61b7b387ae9b0d240d2dcc7c08711
18simran <mewannabeauthentic@gmail.com>
2024-08-25 16:23:36 +0530

    Modified and Added

--- Arrays/BuyandSell.java
+++ Arrays/BuyandSell.java
@@ -1,2 +1,24 @@
-package Arrays;public class BuyandSell {
+package Arrays;
+
+public class BuyandSell
+{
+    public static void main(String[] args)
+    {
+
+        int arr[] = {3,1,4,8,7,2,5};
+        int max_profit = 0;
+        int min = arr[0] ;
+        for(int i =1 ;i <  arr.length ;i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i] ;
+            }
+          else  if(arr[i] - min  > max_profit)
+            {
+               max_profit = arr[i] -min ;
+            }
+        }
+        System.out.println(max_profit);
+    }
 }
--- Arrays/Duplicates.java
+++ Arrays/Duplicates.java
@@ -1,2 +1,21 @@
-package Arrays;public class Duplicates {
+package Arrays;
+
+public class Duplicates {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,6,3,6,1}; // -->1,3,6 repeated elements
+        int aux_arr[] = new int[arr.length] ;
+
+        for (int i= 0 ; i<arr.length ;i++)
+        {
+            if (aux_arr[arr[i]] != 0)
+            {
+                System.out.print(arr[i]+" ");
+            } else {
+                aux_arr[arr[i]]++;
+            }
+
+        }
+
+
+    }
 }
